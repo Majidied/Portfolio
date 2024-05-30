e1caacb8200df58192c438d064210d482b4356cd
Majidied <majidimajidi2003@gmail.com>
2024-05-30 12:45:47 +0100

    feat: Add react-responsive and jquery dependencies
    This commit adds the "react-responsive" and "jquery" dependencies to the project. The "react-responsive" package is added to enable responsive design features, while the "jquery" package is added for general DOM manipulation and event handling. These dependencies enhance the functionality and user experience of the application.

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -16,14 +16,13 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
+      <div className="relative z-0 bg-primary w-full">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
         <About />
         <Experience />
-        <Tech />
         <Works />
         <Feedbacks />
         <div className="relative z-0 bg-[#050816]">
--- Front-End/src/components/Contact.jsx
+++ Front-End/src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Viet Anh Nguyen",
+          to_name: "mohammed majidi",
           from_email: form.email,
-          to_email: "vna1697@gmail.com",
+          to_email: "majidimajidi2003@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
